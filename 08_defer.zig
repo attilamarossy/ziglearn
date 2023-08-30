05b9dee83f38923cf66c43f7a309fb0f479977d9
Attila Marossy <Attila.Marossy@telekom.de>
2023-08-30 22:04:59 +0200

    defer

---
+++ 08_defer.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    var before: i16 = 5;
+    var after: i16 = undefined;
+
+    // multiple defers are executed in the reverse order
+    {
+        defer after *= 2;
+        defer after = before + 2;
+    }
+
+    try stdout.print("Before: {any}, after: {any}\n", .{ before, after });
+}
